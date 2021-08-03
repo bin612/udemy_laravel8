01151a24529fcbe85d697a642d018a6514e43ce5
kimsangbin <tkdqls612@naver.com>
2021-08-03 23:22:10 +0900

    blade 렌더링

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,12 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello World!</h1>
-</body>
-</html>
+{{-- app을 상속 받는다. --}}
+@extends('layouts.app') 
+
+{{-- app  title은 Home page 라고 출력 --}}
+@section('title', 'Home page')
+
+{{-- 섹션을 열고 app을 통하여 Hello world 출력 --}}
+@section('content')
+<h1>Hello world!</h1>
+{{-- 마지막으로 섹션을 끝내야 한다. endsection --}}
+@endsection
