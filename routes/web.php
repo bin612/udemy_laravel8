1644973ca6b550a70c520cf30d45f5b84b68ce59
kimsangbin <tkdqls612@naver.com>
2021-08-05 21:47:32 +0900

    데이터 렌더링/단순 렌더링

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,20 @@ use Whoops\Run;
 
 //view(home(폴더).index(파일))
 // 실제로 홈 디렉토리에있는 인덱스 파일을 읽어 출력 할 수 있다..
-Route::get('/', function () {
-    return view('home.index');
-})->name('home.index');
+// Route::get('/', function () {
+//     return view('home.index');
+// })->name('home.index');
 
-Route::get('/contact', function () {
-    return view('home.contact');
-})->name('home.contact');
+// Route::get('/contact', function () {
+//     return view('home.contact');
+// })->name('home.contact');
+
+// 매개변수가 없고 추가로 작업 할게 없는 페이지들은 정적메소드인 view()를 사용해 주는 것도 좋다.
+Route::view('/','home.index')
+->name('home.index');
+
+Route::view('/contact','home.contact')
+->name('home.contact');
 
 Route::get('/posts/{id}', function ($id) {
     
