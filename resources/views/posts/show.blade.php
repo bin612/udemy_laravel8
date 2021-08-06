c58219a3ff84da94ad42db75e8d5a3f4382c2956
kimsangbin <tkdqls612@naver.com>
2021-08-06 21:17:49 +0900

    blade 조건문

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,6 +3,12 @@
 @section('title', $post['title'])
 
 @section('content')
+@if ($post['is_new'])
+<div>A new blog post! Using if</div>
+{{-- @elseif (!$post['is_new']) --}}
+@else
+<div>Blog post is old! using elseif/else</div>    
+@endif
 <h1>{{ $post['title'] }}</h1>
 <p>{{ $post['content'] }}</p>
 @endsection
