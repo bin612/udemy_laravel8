4feccca9e56649f15aa9b7f5247a321101ab5645
kimsangbin <tkdqls612@naver.com>
2021-08-07 23:42:11 +0900

    루프 내부의 더 많은 제어

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -7,5 +7,24 @@
 {{-- 섹션을 열고 app을 통하여 Hello world 출력 --}}
 @section('content')
 <h1>Hello world!</h1>
+
+<div>
+    @for ($i = 0; $i < 10; $i++)
+        <div>The current value is {{ $i }}</div>
+    @endfor
+</div>
+
+<div>
+    @php $done = false @endphp
+    @while (!$done)
+        <div>I'm not done!</div>
+
+        @php
+            if (random_int(0, 1) === 1) $done = true
+        @endphp
+    @endwhile
+
+</div>
+
 {{-- 마지막으로 섹션을 끝내야 한다. endsection --}}
 @endsection
